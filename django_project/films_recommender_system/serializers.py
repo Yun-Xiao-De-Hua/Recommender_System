3087e7d4f3df5b8024439de2616ea7979ff95ebb
YunXiao <1139529798@qq.com>
2025-09-03 10:42:42 +0800

    创建了 ，定义了数据应该如何被转换成 JSON 格式

--- django_project/films_recommender_system/serializers.py
+++ django_project/films_recommender_system/serializers.py
@@ -30,20 +30,22 @@ class ReviewSerializer(serializers.ModelSerializer):
 # ------ 核心的、用于API端点的Serializer ------
 
 class MovieListSerializer(serializers.ModelSerializer):
+    """用于电影列表页的、轻量级的Serializer"""
     primary_cn_title = serializers.SerializerMethodField()
 
     class Meta:
         model = Movie
-        fields = ['imdb_id', 'original_title','release_year']
+        fields = ['imdb_id', 'primary_cn_title', 'release_year']
 
     def get_primary_cn_title(self,obj):
-        primary_cn_title_obj = obj.titles.filter(is_primary=True).fisrt()
+        primary_cn_title_obj = obj.titles.filter(is_primary=True).first()
         if primary_cn_title_obj:
             return primary_cn_title_obj.title_text
         any_title_obj = obj.titles.filter().first()
         return any_title_obj.title_text if any_title_obj else obj.original_title
 
-class movieDetailSerializer(MovieListSerializer):
+class MovieDetailSerializer(MovieListSerializer):
+    """用于电影详情页的Serializer，继承自列表Serializer"""
     # 添加关联字段
     genres = GenreSerializer(many=True, read_only=True)
     directors = PersonSerializer(many=True, read_only=True)
@@ -61,6 +63,7 @@ class movieDetailSerializer(MovieListSerializer):
         ]
 
 class UserReviewSerializer(serializers.ModelSerializer):
+    """用于处理用户评分和评论的Serializer"""
     user = serializers.ReadOnlyField(source='user.username')
 
     class Meta:
